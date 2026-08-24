dd0cd0e08479e44a984f1aa43fbeb1cf70ffe086
230059 <angel.prieto@alumnos.upm.es>
2026-08-24 12:46:56 +0200

    bug

--- AI/Agent/opponent_pool.py
+++ AI/Agent/opponent_pool.py
@@ -20,13 +20,14 @@ class OpponentPool:
             self.delete_first(first_index)
         path1 = self.path / f"snapshotsSELECTION_{last_index + 1}.pth"
         path2 = self.path / f"snapshotsTURN_{last_index + 1}.pth"
-        for path, (net, target_net, opt, eps_attr, replayed_attr) in zip((path1, path2), player._network_specs()):
+        for path, (net, target_net, opt,replay_memory, eps_attr, replayed_attr) in zip((path1, path2), player._network_specs()):
             torch.save({
                 "dqn": net.state_dict(),
                 "targetdqn": target_net.state_dict(),
                 "optimizer": opt.state_dict(),
                 "epsilon": getattr(player, eps_attr),
                 "replayed_selection": getattr(player, replayed_attr),
+                "replay_memory": replay_memory.state_dict(),
             }, path)
             
     def get_random(self):
