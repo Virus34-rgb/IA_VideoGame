9a89bbfb454893badf4637486959ba7f94688d6f
230059 <angel.prieto@alumnos.upm.es>
2026-08-25 21:48:20 +0200

    train steps

--- mainV.py
+++ mainV.py
@@ -229,8 +229,8 @@ def run_comparison(config: RunConfig, run_specs: list):
 # ==================================================================
 # CONFIGURACIÓN
 # ==================================================================
-VERSION = 2
-N_BATCH = 256
+VERSION = 3
+N_BATCH = 512
 TRAIN_EPISODES = math.ceil(100000 / N_BATCH)
 EVAL_EPISODES = math.ceil(10000 / N_BATCH)
 
