02a02776fcc405641b4d7ad8daa2b18fd6a7c00c
230059 <angel.prieto@alumnos.upm.es>
2026-09-10 00:21:47 +0200

    Fix: remover archivos rar del historial y aplicar gitignore

--- proffileAnalysis.py
+++
@@ -1,9 +0,0 @@
-import pstats
-
-#python .\proffileAnalysis 
-#python -m cProfile -o profile_replay_optimizedv3.prof mainV.py
-print("========== CUMULATIVE ==========")
-pstats.Stats("antes_de_mejoras.prof").sort_stats("cumulative").print_stats(40)
-
-print("\n========== TOTTIME ==========")
-pstats.Stats("antes_de_mejoras.prof").sort_stats("tottime").print_stats(40)
--- run_training.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\18\Insiders\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64
-cd /d "C:\Users\mende\Universidad\AI-Adaptive-Game\CastleGame"
-python mainV.py
-pause
